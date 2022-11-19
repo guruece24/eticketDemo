cfaa8caadd3093a1018f5d229f1e2379eaa841de
guruece24 <guruece24@gmail.com>
2022-11-19 20:46:23 +0530

    producersindex4

--- Controllers/CinemasController.cs
+++ Controllers/CinemasController.cs
@@ -1,9 +1,6 @@
 ﻿using eTickets.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Controllers
